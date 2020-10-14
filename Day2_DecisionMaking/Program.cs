d700424600daad217eb52b31f521fce93d288823
Robert Bizon <m4stersplint3r@gmail.com>
2020-10-14 00:50:28 -0400

    Update to lab 3

--- Day2_DecisionMaking/Program.cs
+++ Day2_DecisionMaking/Program.cs
@@ -5,9 +5,9 @@ namespace Day2_DecisionMaking
 {
     class Program
     {
-        public bool validInputCheck(int x)
+        public static bool validInputCheck(int x)
         {
-            if (x < 0 || x > 100)
+            if (x <= 0 || x > 100)
             {
                 return false;
             }
@@ -22,7 +22,6 @@ namespace Day2_DecisionMaking
             int inputNum;
             bool validInput = false;
             bool continueInput = true;
-            Program program = new Program();
 
             Console.Write("Please enter your name: ");
             username = Console.ReadLine();
@@ -33,7 +32,7 @@ namespace Day2_DecisionMaking
                 
                 if(int.TryParse(Console.ReadLine(), out inputNum))
                 {
-                    validInput = program.validInputCheck(inputNum);
+                    validInput = validInputCheck(inputNum);
                 }
 
                 while (!validInput)
@@ -41,32 +40,32 @@ namespace Day2_DecisionMaking
                     Console.Write($"Really now {username}... Please try again. Enter a number between 1 and 100: ");
                     if (int.TryParse(Console.ReadLine(), out inputNum))
                     {
-                        validInput = program.validInputCheck(inputNum);
+                        validInput = validInputCheck(inputNum);
                     }
                 }
 
                 if (inputNum % 2 != 0 && inputNum > 60)
                 {
-                    Console.WriteLine($"{username} you entered {inputNum} which is an odd number and greater than 60.");
+                    Console.WriteLine($"{username}, you entered {inputNum} which is an odd number and greater than 60.");
                 }
                 else if (inputNum % 2 != 0)
                 {
-                    Console.WriteLine($"{username} you entered {inputNum} which is an odd number.");
+                    Console.WriteLine($"{username}, you entered {inputNum} which is an odd number.");
                 }
                 else if (inputNum % 2 == 0 && inputNum > 60)
                 {
-                    Console.WriteLine($"{username} you entered {inputNum} which is an even number and greater than 60.");
+                    Console.WriteLine($"{username}, you entered {inputNum} which is an even number and greater than 60.");
                 }
                 else if (inputNum % 2 == 0 && inputNum >= 26 && inputNum <= 60)
                 {
-                    Console.WriteLine($"{username} you entered {inputNum} which is an even number and between 25 and 61.");
+                    Console.WriteLine($"{username}, you entered {inputNum} which is an even number and between 25 and 61.");
                 }
                 else if (inputNum % 2 == 0 && inputNum >= 2 && inputNum <= 25)
                 {
-                    Console.WriteLine($"{username} you entered {inputNum} which is an even number and less than 25.");
+                    Console.WriteLine($"{username}, you entered {inputNum} which is an even number and less than 25.");
                 }
 
-                Console.Write(Environment.NewLine + $"{username} would you like to continue? (y/n): ");
+                Console.Write(Environment.NewLine + $"Would you like to continue {username}? (y/n): ");
                 inputYN = Console.ReadLine();
 
                 if (!(inputYN.ToLower().Trim() == "y"))
@@ -77,7 +76,7 @@ namespace Day2_DecisionMaking
                     Console.WriteLine(Environment.NewLine);
                 }
             }
-            Console.WriteLine(Environment.NewLine + "Thanks for using my program! Bye!");
+            Console.WriteLine(Environment.NewLine + $"Thanks for using my program! Bye {username}!");
             
 
         }
